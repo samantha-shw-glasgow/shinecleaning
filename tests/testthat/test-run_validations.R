f599f0bc25499e441537204578087615ed37db60
Erik Igelström <eigelstrom@gmail.com>
2024-07-17 21:46:23 +0100

    Concatenate multiple validation errors in output

--- tests/testthat/test-run_validations.R
+++ tests/testthat/test-run_validations.R
@@ -14,3 +14,26 @@ test_that("validation column is added", {
   expected$errors <- c(NA, NA, "Duplicate DOB", "Duplicate DOB", NA)
   expect_identical(run_validations(input, validators), expected)
 })
+
+test_that("multiple validators work", {
+  validators <- list(check_duplicate_dob, check_duplicate_dob)
+  input <- data.frame(
+    id = 1:5,
+    dob = c(
+      lubridate::ymd("2010-01-01"),
+      lubridate::ymd("2010-01-02"),
+      lubridate::ymd("2010-01-03"),
+      lubridate::ymd("2010-01-03"),
+      lubridate::ymd("2010-01-04")
+    )
+  )
+  expected <- input
+  expected$errors <- c(
+    NA,
+    NA,
+    "Duplicate DOB; Duplicate DOB",
+    "Duplicate DOB; Duplicate DOB",
+    NA
+  )
+  expect_identical(run_validations(input, validators), expected)
+})
