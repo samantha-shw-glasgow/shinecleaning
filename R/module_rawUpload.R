eba49ade176b9ce69a210e909886d048c6a0fc21
Dylan Lewis <d.lewis.1@research.gla.ac.uk>
2024-07-18 20:24:00 +0100

    WIP upload & warnings

--- R/module_rawUpload.R
+++ R/module_rawUpload.R
@@ -7,7 +7,9 @@ rawUploadUI <- function(id){
 	ns <- NS(id)
 
 	tagList(
-		h2("Upload data from Qualtrics")
+		h2("Upload data from Qualtrics"),
+		fileInput(ns("upload"), "Select survey data", buttonLabel = "Upload..."),
+		uiOutput(ns("warn"))
 	)
 }
 
@@ -28,9 +30,38 @@ rawUpload_server <- function(id){
 				ns <- session$ns
 				send_message <- make_send_message(session)
 
-				# your code here
+				# upload data
+				data <- reactive({
+				  req(input$upload)
+				  read.csv(input$upload$datapath)
+				})
 
-				#return(raw_data) ## - must return reactive dataframe
+				# run checks
+				output$warn <- renderUI({
+
+				  req(data(), cancelOutput = TRUE)
+
+				  warnings <- list()
+
+				  if(nrow(data())>1){ #demo check 1
+				    warnings$check1 <- tags$div(span(icon("triangle-exclamation"),
+				                       "This is not the file you are looking for"),
+				                       class="card p-2 bg-danger"
+				                       )
+				  }
+				  if(ncol(data())>1){ #demo check 2
+				    warnings$check2 <-
+				                  tags$div(span(icon("circle-question"),
+				                       "Are you sure this is right?"),
+				                       class="card p-2 bg-warning"
+				                       )
+				  }
+
+				  #combine into taglist
+				  do.call(tagList, warnings)
+				})
+
+				return(data)
 		}
 	)
 }
