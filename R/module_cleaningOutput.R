293b88b31d07dc6d30cd4dfa4b09c63dd5a6c170
Erik Igelström <eigelstrom@gmail.com>
2024-07-29 13:58:11 +0100

    Whitespace formatting

--- R/module_cleaningOutput.R
+++ R/module_cleaningOutput.R
@@ -3,13 +3,13 @@
 #' @param id Unique id for module instance.
 #'
 #' @keywords internal
-cleaningOutputUI <- function(id){
-	ns <- NS(id)
+cleaningOutputUI <- function(id) {
+  ns <- NS(id)
 
-	tagList(
-		h2("Download processed data"),
+  tagList(
+    h2("Download processed data"),
     downloadButton(ns("downloadData"), "Download")
-	)
+  )
 }
 
 #' cleaningOutput Server
@@ -18,33 +18,30 @@ cleaningOutputUI <- function(id){
 #' @param data reactive dataframe, from dataCleaning_server
 #'
 #' @keywords internal
-cleaningOutput_server <- function(id, data){
-	moduleServer(
-		id,
-		function(
-			input,
-			output,
-			session
-			){
+cleaningOutput_server <- function(id, data) {
+  moduleServer(
+    id,
+    function(input,
+             output,
+             session) {
+      ns <- session$ns
+      send_message <- make_send_message(session)
 
-				ns <- session$ns
-				send_message <- make_send_message(session)
+      output$downloadData <- downloadHandler(
+        filename = function() {
+          paste("data-", Sys.Date(), ".xlsx", sep = "")
+        },
+        content = function(file) {
+          wb <- openxlsx::createWorkbook()
+          openxlsx::addWorksheet(wb, "Sheet 1")
+          openxlsx::writeData(wb, 1, data(), withFilter = TRUE)
+          openxlsx::setColWidths(wb, 1, cols = ncol(data()), widths = "auto")
 
-				output$downloadData <- downloadHandler(
-				  filename = function() {
-				    paste("data-", Sys.Date(), ".xlsx", sep="")
-				  },
-				  content = function(file) {
-				    wb <- openxlsx::createWorkbook()
-				    openxlsx::addWorksheet(wb, "Sheet 1")
-				    openxlsx::writeData(wb, 1, data(), withFilter = TRUE)
-				    openxlsx::setColWidths(wb, 1, cols = ncol(data()), widths = "auto")
-
-				    openxlsx::saveWorkbook(wb, file = file)
-				  }
-				)
-		}
-	)
+          openxlsx::saveWorkbook(wb, file = file)
+        }
+      )
+    }
+  )
 }
 
 # UI
