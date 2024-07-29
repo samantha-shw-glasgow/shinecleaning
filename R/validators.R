49c75edf1c4f30ada5552fbe0aa103e8f932add8
Erik Igelström <eigelstrom@gmail.com>
2024-07-29 21:23:14 +0100

    Document validator functions and run devtools::document()

--- R/validators.R
+++ R/validators.R
@@ -1,17 +1,27 @@
+#' Validators
+#' @param data Raw input dataset
+#' @name validators
+#' @returns A character vector of validation messages, in the same order as the rows in the input data
+NULL
+
+#' @rdname validators
 check_duplicate_dob <- function(data) {
   occurrences_lookup <- table(data$dob)
   occurrences <- c(occurrences_lookup[as.character(data$dob)])
   ifelse(occurrences > 1, "Duplicate DOB", "")
 }
 
+#' @rdname validators
 nothing_is_wrong <- function(data) {
   rep("", nrow(data))
 }
 
+#' @rdname validators
 everything_sucks <- function(data) {
   rep("This sucks", nrow(data))
 }
 
+#' @rdname validators
 everything_is_fine <- function(data) {
   rep("This is fine", nrow(data))
 }
