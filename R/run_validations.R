fbe29abc07d44c6596b430fc5cc53c8c79dabc91
Erik Igelström <eigelstrom@gmail.com>
2024-07-29 21:15:57 +0100

    More intelligible code for assembling error messages

--- R/run_validations.R
+++ R/run_validations.R
@@ -1,17 +1,26 @@
 run_validations <- function(data, validators) {
-  results <- lapply(validators, function(fun) {
-    fun(data)
-  })
-  results <- do.call(cbind, results)
-  results <- apply(results, 1, function(vec) {
-    paste(na.omit(vec), collapse = "; ")
-  })
-  names(results) <- NULL
+  messages <- rep("", nrow(data))
+  for (validator_fn in validators) {
+    messages <- append_if_nonempty(
+      messages,
+      validator_fn(data)
+    )
+  }
+  names(messages) <- NULL
   data |>
     dplyr::mutate(
-      "Error messages" = results,
+      "Error messages" = messages,
       "Keep row?" = 1,
       "Reviewer notes" = "",
       .before = 1
     )
+}
+
+append_if_nonempty <- function(string_1, string_2) {
+  dplyr::case_when(
+    string_1 == "" & string_2 == "" ~ "",
+    string_1 != "" & string_2 == "" ~ string_1,
+    string_1 == "" & string_2 != "" ~ string_2,
+    string_1 != "" & string_2 != "" ~ paste(string_1, string_2, sep = "; "),
+  )
 }
--- R/validators.R
+++ R/validators.R
@@ -1,11 +1,11 @@
 check_duplicate_dob <- function(data) {
   occurrences_lookup <- table(data$dob)
   occurrences <- c(occurrences_lookup[as.character(data$dob)])
-  ifelse(occurrences > 1, "Duplicate DOB", NA)
+  ifelse(occurrences > 1, "Duplicate DOB", "")
 }
 
 nothing_is_wrong <- function(data) {
-  rep(NA, nrow(data))
+  rep("", nrow(data))
 }
 
 everything_sucks <- function(data) {
