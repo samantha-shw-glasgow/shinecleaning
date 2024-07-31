46de06319f1ce739009f76371b3233c6ed80eccb
Andy Baxter <andy@andybaxter.me>
2024-07-31 10:38:59 +0100

    add tests for report generation

---
+++ tests/testthat/test-render-reports.R
@@ -0,0 +1,17 @@
+test_that("primary report - sample data", {
+
+  pri_test_a <- readr::read_csv(system.file("testdata", "primary-school-full.csv", package = "SHINEcleaning"))
+  pri_test_b <- readr::read_csv(system.file("testdata", "primary-school-small.csv", package = "SHINEcleaning"))
+
+  out_dir <- tempdir()
+
+  render_report(pri_test_a, school_name = "Test School A", output_location = out_dir, filename = "primary_report_a.docx")
+  render_report(pri_test_b, school_name = "Test School B", output_location = out_dir, filename = "primary_report_b.docx")
+
+  expect_true(
+    file.exists(file.path(out_dir, "primary_report_a.docx")) &
+    file.exists(file.path(out_dir, "primary_report_b.docx"))
+  )
+
+  expect_equal(2 * 2, 4)
+})
