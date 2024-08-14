a280c9ec8ae5861459ee554076f6fe6a78499f18
Erik Igelström <eigelstrom@gmail.com>
2024-08-14 19:32:17 +0100

    Put cleaning rules stuff in a single file

--- R/cleaning_rules.R
+++ R/cleaning_rules.R
@@ -1,3 +1,32 @@
+run_validations <- function(data, validators) {
+  messages <- rep("", nrow(data))
+  should_include <- rep(TRUE, nrow(data))
+  for (validator_fn in validators) {
+    result <- validator_fn(data)
+    messages <- append_if_nonempty(
+      messages,
+      result$message
+    )
+    should_include <- should_include & result$include
+  }
+  data |>
+    dplyr::mutate(
+      "Error messages" = messages,
+      "Keep row?" = as.numeric(should_include),
+      "Reviewer notes" = "",
+      .before = 1
+    )
+}
+
+append_if_nonempty <- function(string_1, string_2) {
+  dplyr::case_when(
+    string_1 == "" & string_2 == "" ~ "",
+    string_1 != "" & string_2 == "" ~ string_1,
+    string_1 == "" & string_2 != "" ~ string_2,
+    string_1 != "" & string_2 != "" ~ paste(string_1, string_2, sep = "; "),
+  )
+}
+
 #' Validators
 #' @param data Raw input dataset
 #' @name validators
--- tests/testthat/test-cleaning_rules.R
+++ tests/testthat/test-cleaning_rules.R
@@ -33,3 +33,22 @@ test_that("columns are added with multiple validators", {
   result <- run_validations(input, validators)
   expect_identical(result, expected)
 })
+
+test_validator_with_data <- function(validator_fun, filename) {
+  data <- readr::read_csv(
+    file.path("examples", filename),
+    na = character(),
+    show_col_types = FALSE
+  )
+  result <- validator_fun(data)
+  expect_equal(result$include, data$expected_include)
+  expect_equal(result$message, data$expected_message)
+}
+
+test_that("duration_too_short", {
+  test_validator_with_data(duration_too_short, "duration_too_short.csv")
+})
+
+test_that("no_test_responses", {
+  test_validator_with_data(no_test_responses, "no_test_responses.csv")
+})
