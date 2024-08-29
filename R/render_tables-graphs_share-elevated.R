1a53c7bb771640a66eacfde2f29c8ce8d87e0691
Dylan Lewis <d.lewis.1@research.gla.ac.uk>
2024-08-29 20:11:20 +0100

    add genders argument. move labelling to graph function

--- R/render_tables-graphs_share-elevated.R
+++ R/render_tables-graphs_share-elevated.R
@@ -5,13 +5,14 @@
 #' @param .censor `TRUE`/`FALSE` - apply censoring rules (must be `TRUE` in output reports)
 #' @param .split Split columns by gender x class
 #' @param classes Vector names of classes
+#' @param genders Vector names of genders
 #'
 #' @return dataframe
 #' @export
 #'
 #' @examples
 
-share_elevated <- function(data, .split, .censor = TRUE, classes = "All") {
+share_elevated <- function(data, .split, .censor = TRUE, classes = "All", genders = c("Boy", "Girl")) {
 
   clean_dat <- data |>
     dplyr::summarise(
@@ -20,7 +21,8 @@ share_elevated <- function(data, .split, .censor = TRUE, classes = "All") {
       denom = sum(!is.na(mme_cat))
     ) |>
     dplyr::mutate(
-      labels = "All")
+      gender = "All",
+      class = "All")
 
   if (.split) {
     split_dat <- data |>
@@ -30,33 +32,25 @@ share_elevated <- function(data, .split, .censor = TRUE, classes = "All") {
         expected = sum(mme_cat %in% c("Expected"), na.rm = TRUE),
         denom = sum(!is.na(mme_cat))
       ) |>
-      dplyr::mutate(
-        labels = dplyr::if_else(
-          class %in% classes & gender %in% c("Boy", "Girl"),
-          stringr::str_c(class, " ", gender, "s"),
-          NA
-        )
-      ) |>
-      dplyr::filter(!is.na(labels)) |>
-      ungroup() |>
-      select(-c("gender", "class"))
+      dplyr::filter(class %in% classes,
+                    gender %in% genders) |>
+      ungroup()
 
     clean_dat <- dplyr::bind_rows(clean_dat, split_dat)
   }
 
   graph_dat <- clean_dat |>
     mutate(censored = if_else(denom < 3 & .censor, 1, 0)) |>
-    pivot_longer(-c(labels, denom, censored),
+    pivot_longer(-c(denom, censored, gender, class),
                  names_to = "var",
                  values_to = "n") |>
     mutate(prop = if_else(censored == 1, 1, n / denom),
            var = case_when(
              var == "elevated" ~ "Elevated",
              var == "expected" ~ "As expected"
-           ),
-           labels = forcats::fct_relevel(labels, "All", after = Inf)
+           )
     ) |>
-    select(labels, var, n, denom, prop, censored)
+    select(gender, class, var, n, denom, prop, censored)
 
   return(graph_dat)
 }
@@ -72,6 +66,11 @@ share_elevated <- function(data, .split, .censor = TRUE, classes = "All") {
 bar_share_elevated <- function(graph_data) {
   graph_dat <- graph_data |>
     mutate(
+      x_lab = if_else(
+        class == "All" & gender == "All",
+        "All",
+        stringr::str_c(class, " ", gender, "s")
+      ) |> forcats::fct_relevel("All", after = Inf),
       bar_lab_main = if_else(
         censored == 1,
         "*",
@@ -80,7 +79,6 @@ bar_share_elevated <- function(graph_data) {
     )
 
   ggplot(data = graph_dat,
-         aes(x = labels, y = prop, fill = var)) +
     geom_bar(stat = "identity", position = "stack") +
     scale_fill_hbsc(name = "") +
     scale_y_continuous("", labels = scales::percent) +
