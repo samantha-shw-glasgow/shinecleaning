2671f4865184e44eed65bd3094b277958d25519e
Andy Baxter <andy@andybaxter.me>
2024-09-12 21:58:14 +0100

    tidied args and documented

--- R/report_mean-single.R
+++ R/report_mean-single.R
@@ -1,34 +1,32 @@
-#' Bar graph of means for a single variable
+#' Summary table of means for a single variable
 #'
 #' @param data Valid input data
 #' @param var Variable to calculate mean of
+#' @param genders List of genders to split by
+#' @param classes List of classes to split by
 #' @param .censor `TRUE`/`FALSE` - apply censoring rules (must be `TRUE` in output reports)
 #' @param .gender_split `TRUE`/`FALSE` - split by gender when sufficient numbers of responses
-#' @param limits Vector of upper/lower score limits.
-#' @param ymax Upper limit of graph (deaults to `limits[2]`)
-#' @param ylab Label for X axis (summary statistic, i.e. "mean")
-#' @param classes Vector names of classes to filter/combine.
 #'
-#' @return A ggplot2 bar graph of mean of a scoring variables
+#' @return A summary table of means of a single variable
 #'
 #' @import dplyr
-#' @import ggplot2
 #' @importFrom rlang .data
 #'
 summary_mean_single_var <-
   function(data,
            var,
+           genders = c("Boys", "Girls"),
+           classes = "All",
            .censor = TRUE,
-           .gender_split = TRUE,
-           classes = "All") {
+           .gender_split = TRUE
+           ) {
     subgroups <- tibble()
 
     if (.gender_split) {
       subgroups <-
         map(classes, \(concat_class) {
           data |>
-            # Can this filter for two sets of groupings - class concat or expand?
-            filter(gender %in% c("Boys", "Girls"), class %in% concat_class) |>
+            filter(gender %in% genders, class %in% concat_class) |>
             summarise(
               mean_score = mean({{var}}, na.rm = TRUE),
               class = str_flatten(concat_class, collapse = ", ", last = " and "),
@@ -47,10 +45,18 @@ summary_mean_single_var <-
 
     bind_rows(subgroups, all) |>
       mutate(bar_lab_main = sprintf("%.1f", mean_score),
-             class = fct_inorder(class))
+             class = fct_inorder(class)) |>
+      arrange(gender, class)
 
   }
 
+#' Bar graph of mean of a single var
+#'
+#' @param summary_data A dataframe produced by `summary_mean_single`
+#' @param ymax Upper limit of graph (deaults to `limits[2]`)
+#' @param ylab Label for X axis (summary statistic, i.e. "mean")
+#'
+#' @returns A ggplot2 graph
 bar_mean_single <- function(summary_data, ymax, ylab = "Mean") {
 
   summary_data |>
