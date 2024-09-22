2e4efad1fe4b322d1694239530387486b6de6649
Dylan Lewis <d.lewis.1@research.gla.ac.uk>
2024-09-22 09:44:16 +0100

    Finish qualtrics dl guidance

--- R/module_help.R
+++ R/module_help.R
@@ -11,18 +11,24 @@ helpUI <- function(id){
 		  h2("Help"),
 		  bslib::card_body(
 		    bslib::accordion(
+		      open = FALSE,
 		      id = ns("help"),
 		      bslib::accordion_panel(
 		        title = "Downloading data from Qualtrics",
-		        h4("Step 1 - Use the 'Data & Analysis' tab to filter responses to date range"),
-		        img(src = "img/qualtrics_1.jpeg", class = "help-img"),
-		        img(src = "img/qualtrics_2.jpeg", class = "help-img"),
-		        h4("Step 2 - Select 'Export Data...' from the 'Export & Import' menu"),
-		        img(src = "img/qualtrics_3.jpeg", class = "help-img"),
-		      ),
+		        bslib::card_body(
+		          h4("Step 1 - Use the 'Data & Analysis' tab to filter responses to date range"),
+		          img(src = "img/qualtrics_1.jpeg", class = "help-img"),
+		          img(src = "img/qualtrics_2.jpeg", class = "help-img"),
+		          h4("Step 2 - Select 'Export Data...' from the 'Export & Import' menu"),
+		          img(src = "img/qualtrics_3.jpeg", class = "help-img"),
+		          h4("Step 3 - Choose 'CSV' as the file format, ensure the settings under 'More options' match the images below, and click 'Download'"),
+		          img(src = "img/qualtrics_4.jpeg", class = "help-img"),
+		          img(src = "img/qualtrics_5.jpeg", class = "help-img")
+		          )
+		        ),
 		      bslib::accordion_panel(
 		        title = "Data cleaning rules",
-		        "Placeholder text."
+		        "Coming soon."
 		      ),
 		    )
 		  )
