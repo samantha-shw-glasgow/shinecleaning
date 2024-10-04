3da40bbc146c3f2bc3cf55e5b46b8b8d02c06f53
Andy Baxter <andrewbaxter439@gmail.com>
2024-10-04 14:01:23 +0100

    two further tests for rendering half schools

--- tests/testthat/test-render_render-reports.R
+++ tests/testthat/test-render_render-reports.R
@@ -35,6 +35,45 @@ test_that("secondary report - sample data", {
   expect_true(file.exists(file.path(out_dir, "secondary_report_a.docx")))
 })
 
+test_that("primary report - missing class", {
+  pri_test_a <- readr::read_csv(test_path("raw_data", "pri_test_small.csv"),
+    show_col_types = FALSE
+  )[-1:-2, ] |>
+    filter(class == "P7")
+
+  out_dir <- tempdir()
+
+
+  render_report(
+    pri_test_a,
+    survey_type = "primary",
+    school_name = "Test School A",
+    output_location = out_dir,
+    filename = "primary_report_b.docx"
+  )
+  expect_true(
+    file.exists(file.path(out_dir, "primary_report_b.docx"))
+  )
+})
+
+test_that("secondary report - missing upper classes", {
+  sec_test_a <- readr::read_csv(test_path("raw_data", "sec_test_large.csv"),
+                                show_col_types = FALSE)[-1:-2, ] |>
+    filter(class %in% c("S1", "S2", "S3"))
+
+  out_dir <- tempdir()
+
+  render_report(
+    sec_test_a,
+    survey_type = "secondary",
+    school_name = "Test School A",
+    output_location = out_dir,
+    filename = "secondary_report_b.docx"
+  )
+
+  expect_true(file.exists(file.path(out_dir, "secondary_report_b.docx")))
+})
+
 
 test_that("Expects school or LA name", {
   expect_error(render_report(school_name = "a", local_authority_name = "b"))
