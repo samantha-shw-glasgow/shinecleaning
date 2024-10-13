580386c2e270fcf16cb9e5b5f32e4bd7956ae702
Erik Igelström <eigelstrom@gmail.com>
2024-10-13 16:49:49 +0100

    Use same date format as Qualtrics in tests

--- R/cleaning_rules.R
+++ R/cleaning_rules.R
@@ -144,7 +144,7 @@ calculate_expected_class <- function(data) {
   data |>
     dplyr::mutate(
       dob_ym = lubridate::ym(paste(dobyr, dobmnth)),
-      current_year = lubridate::dmy_hm(RecordedDate),
+      current_year = lubridate::ymd_hms(RecordedDate),
       school_dob = lubridate::year(dob_ym - months(8)),
       school_year = lubridate::year(current_year - months(7)),
       expected_class_num = school_year - school_dob,
