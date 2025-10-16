e49d685388a4bdb0f7c4b3b43ff2ada9c3786fce
Andy Baxter <andrewbaxter439@gmail.com>
2025-10-16 13:40:44 +0100

    add alternative DOB checking for new forms

--- R/upload_parsing.R
+++ R/upload_parsing.R
@@ -4,3 +4,25 @@ parse_raw_csv <- function(path) {
                         show_col_types = F
   )[-1:-2, ]
 }
+
+
+read_dob_column <- function(data) {
+
+  data_out <- data
+
+  if (!all(c("dobyr", "dobmnth", "dobday") %in% colnames(data))) {
+
+    if ("dob_1" %in% colnames(data))
+
+      data_out <- data |>
+      dplyr::mutate(
+        dobyr = lubridate::year(.data$dob_1),
+        dobmnth = lubridate::month(.data$dob_1),
+        dobday = lubridate::day(.data$dob_1)
+      )
+
+  }
+
+  data_out
+
+}
