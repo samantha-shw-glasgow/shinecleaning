4b8aa5f897130a779fdb0c6c5fa130452b185c50
Andy Baxter <andrewbaxter439@gmail.com>
2025-10-27 23:57:34 +0000

    parsing  birthday formats

--- R/upload_parsing.R
+++ R/upload_parsing.R
@@ -6,23 +6,42 @@ parse_raw_csv <- function(path) {
 }
 
 
+
 read_dob_column <- function(data) {
+  # Surveys in 24-25 year have `dobyr`, `dobmnth` and `dobday`
+  # Surveys from Oct '25 have `dob_1` in place of above
+  # Surveys from Nov '25 have `dob_2#1_1`, `dob_2#2_1`, `dob_2#3_1` also
+  # Only one of the latter two will be completed if present
 
   data_out <- data
 
   if (!all(c("dobyr", "dobmnth", "dobday") %in% colnames(data))) {
+    # If data contains `dob_1`, this indicates survey is Oct '25 pilots
+    if ("dob_1" %in% colnames(data)) {
+      # If data contains `dob_2`, this indicates survey is beyond Oct '25 pilots
+      if (all(is.na(data$dob_1)) &
+          "dob_2#1_1" %in% colnames(data)) {
+        data_out <- data |>
+          dplyr::mutate(
+            dobyr = .data$`dob_2#3_1`,
+            dobmnth = .data$`dob_2#2_1`,
+            dobday = .data$`dob_2#1_1`
+          )
 
-    if ("dob_1" %in% colnames(data))
+      } else {
+        data_out <- data |>
+          dplyr::mutate(
+            dobyr = lubridate::year(.data$dob_1),
+            dobmnth = lubridate::month(.data$dob_1),
+            dobday = lubridate::day(.data$dob_1)
+          )
+      }
 
-      data_out <- data |>
-      dplyr::mutate(
-        dobyr = lubridate::year(.data$dob_1),
-        dobmnth = lubridate::month(.data$dob_1),
-        dobday = lubridate::day(.data$dob_1)
-      )
 
+    }
   }
 
-  data_out
+  data_out |>
+    dplyr::select(-dplyr::starts_with("dob_"))
 
 }
